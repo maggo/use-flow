1a5d9b299322a01462d7f766d04af85b2caefc0c
maggo <me@mar.co.de>
2022-07-05 18:13:25 +0200

    feat(hook): Add useScript hook and example
    This adds a hook to abstract FCLs query function. The user passes cadence code,
    arguments in function form and a query key as parameters.
    Custom error and argument handling needs to be refined in a further update.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export { createClient, FlowProvider } from "./FlowProvider";
 export * as networks from "./networks";
 export { useAuthentication } from "./useAuthentication";
+export { useScript } from "./useScript";
