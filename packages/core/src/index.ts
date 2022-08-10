5aafe929e20941c540062e36fb48bafb978da6c6
maggo <me@mar.co.de>
2022-08-10 19:28:59 +0200

    feat: Implement better arguments typing

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,5 @@ export { createClient, FlowProvider } from "./FlowProvider";
 export * as networks from "./networks";
 export { useAuthentication } from "./useAuthentication";
 export { useScript } from "./useScript";
+
+export { ArgumentFunction } from "./arguments";
