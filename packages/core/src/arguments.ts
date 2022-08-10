a1388bc7de949750600ebd67634407d49a537c4c
maggo <me@mar.co.de>
2022-08-10 19:28:59 +0200

    feat: Implement better arguments typing

--- packages/core/src/arguments.ts
+++ packages/core/src/arguments.ts
@@ -1,3 +1,52 @@
-// @TODO: Refactor to better argument type system */
-export type Argument = [any, any];
-export type ArgumentFunction = (arg: Function, t: any) => Argument[];
+/** @TODO Refactor to better argument type system */
+
+export type ArgumentFunction = (arg: ArgFunc, t: FType) => any[];
+
+type ArgFunc<
+  T extends any = any,
+  F extends FType<T>[keyof FType] = FType<T>[keyof FType]
+> = (value: T, xform: F) => ArgumentObject<T, F>;
+
+type ArgType<T extends any = any> = (
+  label: string,
+  asArgument: (value: T) => any
+) => { label: string; asArgument: (value: T) => any };
+
+type ArgumentObject<Value extends any, Form extends FType[keyof FType]> = {
+  value: Value;
+  xform: Form;
+};
+
+interface FType<T extends any = any> {
+  UInt: ArgType<string>;
+  Int: ArgType<string>;
+  UInt8: ArgType<string>;
+  Int8: ArgType<string>;
+  UInt16: ArgType<string>;
+  Int16: ArgType<string>;
+  UInt32: ArgType<string>;
+  Int32: ArgType<string>;
+  UInt64: ArgType<string>;
+  Int64: ArgType<string>;
+  UInt128: ArgType<string>;
+  Int128: ArgType<string>;
+  UInt256: ArgType<string>;
+  Int256: ArgType<string>;
+  Word8: ArgType<string>;
+  Word16: ArgType<string>;
+  Word32: ArgType<string>;
+  Word64: ArgType<string>;
+  UFix64: ArgType<string>;
+  Fix64: ArgType<string>;
+  String: ArgType<string>;
+  Character: ArgType<string>;
+  Bool: ArgType<boolean>;
+  Address: ArgType<string>;
+  Optional: (value: ArgType<T>) => ArgType<T>;
+  Array: (value: ArgType<T>) => ArgType<T>;
+  Dictionary: (value: { key: ArgType; value: ArgType }) => ArgType;
+  Path: (value: {
+    domain: "public" | "private" | "storage";
+    identifier: string;
+  }) => ArgType<string>;
+}
