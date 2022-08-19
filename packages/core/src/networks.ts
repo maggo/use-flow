785991cf4b48e09782f0658be36965d7c4dee293
maggo <me@mar.co.de>
2022-08-19 02:40:19 +0200

    feat: Add default network configs
    This adds configs with common access node URLs that can be used as-is or extendend

---
+++ packages/core/src/networks.ts
@@ -0,0 +1,39 @@
+import { FCLConfig } from "./FlowProvider";
+
+/**
+ * Default access node and wallet discovery config for mainnet
+ */
+export const mainnet: FCLConfig = {
+  "accessNode.api": "https://rest-mainnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "flow.network": "mainnet",
+};
+
+/**
+ * Default access node and wallet discovery config for testnet
+ */
+export const testnet: FCLConfig = {
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/mainnet/authn",
+  "flow.network": "testnet",
+};
+
+/**
+ * Default access node and wallet discovery config for canarynet
+ */
+export const canarynet: FCLConfig = {
+  "accessNode.api": "https://rest-canarynet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/canarynet/authn",
+  "flow.network": "canarynet",
+};
+
+/**
+ * Default access node and wallet discovery config for local emulator network
+ *
+ * @warning Requires the flow emulator network and flow dev-wallet to be running
+ */
+export const local: FCLConfig = {
+  "accessNode.api": "http://localhost:8888",
+  "discovery.wallet": "http://localhost:8701/fcl/authn",
+  "flow.network": "local",
+};
